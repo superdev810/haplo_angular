f0c10e9f390a44f59565d070131d686315d4080b
metaphor10 <ariel.borochov@gmail.com>
2017-06-26 00:01:35 -0400

    adding express-session and passport

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'hello'});
+});
+
+router.post('/signup', function(req, res, next) {
+  console.log(req.body.username);
+  console.log(req.body.password);
+  res.render('index', {title: 'hello'})
 });
 
 module.exports = router;
