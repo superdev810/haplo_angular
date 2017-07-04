17e55d643b563587e5f4cb641366d552dddc18dc
metaphor10 <ariel.borochov@gmail.com>
2017-07-03 22:56:24 -0400

    adding eslint

--- test/app_spec.js
+++ test/app_spec.js
@@ -1,30 +1,27 @@
 const request = require('supertest'),
     express = require('express'),
-    rewire = require('rewire'),
-    index = require('../app.js'),
-    nock = require('nock'),
-    path = require('path');
+    index = require('../app.js');
 
-describe('The Express App ', () => {
-  let app;
-  before(()=> {
-    app = express();
-    app.use(index)
-  })
-  it('404s when the wrong route is requested ', () => {
-    return request(app)
-    .get('/hello')
-    .expect(404)
-  })
+describe('The Express App ', () => { //eslint-disable-line no-undef
+    let app;
+    before(()=> { //eslint-disable-line no-undef
+        app = express();
+        app.use(index);
+    });
+    it('404s when the wrong route is requested ', () => {  //eslint-disable-line no-undef
+        return request(app)
+            .get('/hello')
+            .expect(404);
+    });
 
-  it('404s when the wrong route is requested ', () => {
-    process.env.NODE_ENV='development';
-    return request(app)
-    .get('/heel/all/what')
-    .expect(404)
-  })
+    it('404s when the wrong route is requested ', () => { //eslint-disable-line no-undef
+        process.env.NODE_ENV='development';
+        return request(app)
+            .get('/heel/all/what')
+            .expect(404);
+    });
 
-  it('500s when the route is not defined', () => {
+    it('500s when the route is not defined', () => { //eslint-disable-line no-undef
     // still need to figure it out
-  })
-})
+    });
+});
