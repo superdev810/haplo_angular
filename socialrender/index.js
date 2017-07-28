abfe7d96a5092e405b7e9fa9aecd7169930acfe4
superdev810 <superdev810@gmail.com>
2017-07-29 02:32:59 +1000

    remove social share

--- socialrender/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by superdev on 7/28/2017.
- */
-var express = require('express');
-var app = express();
-app.get('/', function(req, res) {
-  res.send('hello!');
-});
-module.exports = app;
