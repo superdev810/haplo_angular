c1c617fac8b5d9bfa7acdfcd20bc7578460b15d5
superdev810 <superdev810@gmail.com>
2017-07-29 04:50:05 +1000

    social meta tag from server side

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var socialShare = {
+    appId: '2231777543',
+    title:'ustadium',
+    description: 'This is ustadium website for sports fan',
+    siteName: 'Nextgen social sports website',
+    url: 'https://ustadium-webapp.herokuapp.com',
+    image: 'http://ustadium-media.s3.amazonaws.com/content/images/83/5bfaa0c56911e685d8934a6a5ce0af/small.jpg',
+    type: 'website'
+  }
+  res.render('index', { socialShare: socialShare });
 });
 
 module.exports = router;
