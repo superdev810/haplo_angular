c86acfe50320938cb7140d3805568fa28ce7f7a6
aboroch <aboroch@us.ibm.com>
2017-07-29 23:54:22 -0400

    express routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,76 +11,65 @@ var socialShare = {
   url: 'https://ustadium-webapp.herokuapp.com',
   image: 'http://ustadium-media.s3.amazonaws.com/content/images/83/5bfaa0c56911e685d8934a6a5ce0af/small.jpg',
   type: 'website'
-};
+},
+  feedType = {
+    New: '/',
+    Hot: '/trending',
+    subscribed: '/subscribed',
+    created: '/created'
+  };
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  var req_url = req.baseUrl;
-  console.log(req_url.indexOf('/feed/'));
-
-  switch (checkUrl(req_url)){
-    case 'SingleFeed':
-      console.log('SINGLE FEED URL PARSE: ', decodeURI(req_url.split('/')[2]));
-      var feed_name = decodeURI(req_url.split('/')[2]);
-      requestFeedInfo(feed_name, res);
-      break;
-    case 'SinglePost':
-      console.log('SINGLE POST URL PARSE: ', decodeURI(req_url.split('/')[2]));
-      var post_id = req_url.split('/')[2];
-      res.render('index', { socialShare: socialShare});
-      break;
-    default:
-      res.render('index', { socialShare: socialShare});
-      break;
-  }
+router.get('/feeds/:name', function(req, res, next) {
+  res.locals.socialShare = socialShare;
+  var single = '';
+  requestFeedInfo(req.params.name, single, res, next)
+}, function(req, res, next) {
+  console.log(res.locals.socialShare);
+  res.render('index', { socialShare: res.locals.socialShare });
 });
 
-/* GET single feed page */
 router.get('/feed/:name', function(req, res, next) {
-  console.log(req.params);
-  // var socialShare = {
-  //   appId: '2231777543',
-  //   title:'ustadium',
-  //   description: 'This is ustadium website for sports fan',
-  //   siteName: 'Nextgen social sports website',
-  //   url: 'https://ustadium-webapp.herokuapp.com',
-  //   image: 'http://ustadium-media.s3.amazonaws.com/content/images/83/5bfaa0c56911e685d8934a6a5ce0af/small.jpg',
-  //   type: 'website'
-  // }
-  // res.render('index', { socialShare: socialShare });
+  res.locals.socialShare = socialShare;
+  var single = '/name/'
+  requestFeedInfo(req.params.name, single, res, next)
+}, function(req, res, next) {
+  res.render('index', { socialShare: res.locals.socialShare });
 });
 
-function checkUrl(url) {
-  if(url.indexOf('/feed/') != -1){
-    return 'SingleFeed';
-  } else if(url.indexOf('/feeds/Hot') != -1) {
-    return 'HotFeeds';
-  } else if(url.indexOf('/feeds/New') != -1) {
-    return 'HotFeeds';
-  } else if(url.indexOf('/feeds/Subscribed') != -1) {
-    return 'HotFeeds';
-  } else if(url.indexOf('/feeds/Created') != -1) {
-    return 'HotFeeds';
-  } else if(url.indexOf('/post/') != -1) {
-    return 'SinglePost';
-  }else{
-    return null;
-  }
+router.get('/post/:id', function(req, res, next) {
+  res.render('index', { socialShare: socialShare });
+})
 
-}
+router.get('/', function(req, res, next) {
+  res.render('index', { socialShare: socialShare });
+});
 
-function requestFeedInfo(feedName, res) {
+function requestFeedInfo(feedName, single, res, next) {
+  var isSingle = !single ? feedType[feedName] : feedName;
+  var requestEnd = '' + single + isSingle;
   request({
-    uri: 'https://ustadium-api-dev.herokuapp.com/api/feeds/name/' + feedName,
+    uri: 'https://ustadium-api-dev.herokuapp.com/api/feeds'+ requestEnd ,
     method: 'GET'
   }, function (error, response, feed) {
     var feedJson = JSON.parse(feed);
     console.log(feedJson);
-    socialShare.image = feedJson.data.mediaFileThumbnail;
-    socialShare.title = feedJson.data.name;
-    socialShare.description = feedJson.data.description;
-    socialShare.url = feedJson.data.mediaFileThumbnail;
-    res.render('index', {socialShare: socialShare});
+    if (feedJson.data.mediaFileThumbnail) {
+      res.locals.socialShare.image = feedJson.data.mediaFileThumbnail;
+    }
+
+    if (feedJson.data.name) {
+      res.locals.socialShare.title = feedJson.data.name;
+    }
+
+    if (feedJson.data.description) {
+      res.locals.socialShare.description = feedJson.data.description;
+    }
+
+    if(feedJson.data.mediaFileThumbnail) {
+      res.locals.socialShare.url = feedJson.data.mediaFileThumbnail;
+    }
+    next();
   })
+
 }
 module.exports = router;
