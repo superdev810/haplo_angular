92ba5e76cf4b80c8d417ff9a4d8ac79f01a73490
superdev810 <superdev810@gmail.com>
2017-09-06 11:25:35 +1000

    add temrs and service page

--- src/pages/pages.js
+++ src/pages/pages.js
@@ -1,14 +1,18 @@
 angular.module('ustadium.pages', ['ustadium.grandfather'])
-.config(function ($stateProvider) {
-  $stateProvider
-    .state('app.admin', {
-      url: '/admin',
-      templateUrl: 'pages/admin.tpl.html',
-      accessLevel: accessLevels.admin
-    })
-    .state('app.user', {
-      url: '/user',
-      templateUrl: 'pages/user.tpl.html',
-      accessLevel: accessLevels.user
-    });
-});
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('app.admin', {
+        url: '/admin',
+        templateUrl: 'pages/admin.tpl.html',
+        accessLevel: accessLevels.admin
+      })
+      .state('app.user', {
+        url: '/user',
+        templateUrl: 'pages/user.tpl.html',
+        accessLevel: accessLevels.user
+      })
+      .state('app.terms', {
+        url: '/terms-and-service',
+        templateUrl: 'pages/terms_and_service.tpl.html',
+      });
+  });
